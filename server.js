a7f68de2723ceb5ef5906b9aff7eb5c8a35aae2a
joao <joaoafonsoassumpcao@gmail.com>
2017-09-28 11:43:05 -0300

    atualizacao

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+
+var app = express();
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.disable('x-powered-by');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+//more imports here
+
+
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.get('/home', function(req, res){
+    res.render('home');
+});
+
+app.get('/about', function(req, res){
+    res.render('about');
+});
+
+app.listen(app.get('port'), function(){
+    console.log("Express started on http://localhost:" + app.get('port') + " press ctrl c to terminate.");
+});
